b14491ee2fe2ad5f553f782b04246e956d4abf04
waynekiprotich <waynekip123@icloud.com>
2026-06-28 12:23:59 +0300

    Add Analytics component

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,9 @@ import './index.css'
 import App from './App.jsx'
 import { Analytics } from "@vercel/analytics/react";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
     <Analytics />
-  </StrictMode>,
-)
+  </StrictMode>
+);
